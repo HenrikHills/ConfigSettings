fdf6c27587a6688fa74ccefd32c901359d11fa41
Henrik Hills <hto017@student.uib.no>
2020-04-03 16:51:40 +0200

    Changed duplicateremover to be universal

--- Pi-hole/duplicate_remover.py
+++ Pi-hole/duplicate_remover.py
@@ -1,12 +1,22 @@
-dirty=open("C:\Users\henri\Desktop\list.txt", "r")
-fugly = dirty.read()
+import sys
+import os
+import inspect
 
-with open("C:\Users\henri\Desktop\list.txt") as f:
+user_input = raw_input("Enter the path of your file:\n")
+
+assert os.path.exists(user_input), "\nI did not find the file at, "+str(user_input)
+
+with open(user_input) as f:
      content = f.readlines()
 
 clean = list(dict.fromkeys(content))
 clean.sort()
 
-with open('C:\Users\henri\Desktop\\no_duplicates.txt', 'w') as filehandle:
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+with open(path + '\\no_duplicates.txt', 'w') as filehandle:
     for listitem in clean:
         filehandle.write('%s' % listitem)
+
+print("Saved file with no duplicates here: "+ path +"\\no_duplicates.txt")
