01091b6b566d168cb1c2a708a11fdf09eefde5ae
Henrik Hills <hto017@student.uib.no>
2020-09-01 20:54:47 +0200

    massive cleanup of duplicate urls

--- Pi-hole/duplicate_remover.py
+++ Pi-hole/duplicate_remover.py
@@ -6,26 +6,33 @@ Tk().withdraw()
 user_input = askopenfilename()
 
 with open(user_input, encoding="utf-8") as f:
-     content = f.readlines()
+     oldList = f.readlines()
 
+bad_words = [' ']
 
-content = [x for x in content if not x.startswith('#')]
+oldList = [x for x in oldList if not x.startswith('#')]
 
-decision = input("Remove \"0.0.0.0\" from list? Y/N\n")
+decision = input("Remove \"0.0.0.0\" and spaces from list? Y/N \n")
 if decision.upper() == "Y":
     print("Removing ... ")
-    content = [w.replace("0.0.0.0", "").replace('\t', "").replace("127.0.0.1", "") for w in content]
+    oldList = [w.replace("0.0.0.0", "").replace('\t', "").replace("127.0.0.1", "") for w in oldList]
 else:
     print("Not removing \"0.0.0.0\"")
 
-clean = list(dict.fromkeys(content))
-clean.sort()
+
+
+# Make new clean list
+newList = list(dict.fromkeys(oldList))
+newList.sort()
 
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
 
 with open(path + '\\no_duplicates.txt', 'w', encoding="utf-8") as filehandle:
-    for listitem in clean:
-        filehandle.write('%s' % listitem)
+    for line in newList:
+
+        # Remove all lines with spaces in them (many are duplicate...)
+        if not any(bad_words in line for bad_words in bad_words):
+            filehandle.write('%s' % line)
 
 print("Saved file with no duplicates here: \t"+ path +"\\no_duplicates.txt")
